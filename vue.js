9a3eb9b8ff09296bef766c56c596f46ba6cf7b57
Arley David Ramírez Mejia <al03022140@tecmilenio.mx>
2025-11-12 13:25:31 -0600

    Add vue.js re-export

---
+++ vue.js
@@ -0,0 +1,5 @@
+// Archivo mínimo para Vue 3 en entorno browser sin bundler
+// Reexporta desde el build ESM compatible con navegador dentro de node_modules.
+// No monta ninguna app ni modifica el DOM.
+
+export * from './node_modules/vue/dist/vue.esm-browser.js';
